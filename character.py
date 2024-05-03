2094bf261ff261072d7a5ea765cd59891f86ac3f
ryley <ryleycroucher@gmail.com>
2024-05-03 18:14:49 +1000

    initiate chatacter.py

---
+++ character.py
@@ -0,0 +1,5 @@
+class Character:
+    def __init__(self, name: str, health: int, max_health=100):
+        self.name = name
+        self.health = health
+        self.health_max = max_health
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from log_in import start
 
 def run():
+    # Start the program and prompt user to log in or create a new user
     start()
 
 
